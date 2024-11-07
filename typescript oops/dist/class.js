f034264a00746de10057f4ffac3c7e9bb13e0ff0
nxvtej <nxvtej@outlook.com>
2024-11-07 17:22:25 +0530

    another class

--- typescript oops/dist/class.js
+++ typescript oops/dist/class.js
@@ -1,9 +1,29 @@
 "use strict";
-class one {
-    constructor(name) {
-        this._name = name;
+class carCompany {
+    constructor(name, originCountry, establishedYear) {
+        this.name = name;
+        this.originCountry = originCountry;
+        this.establishedYear = establishedYear;
     }
-    get name() {
-        return this._name;
+    // cant do it like this, must have only one agrument
+    /*
+    set newModel(modelName: string, modelPrice: number) {
+        this.modelName = modelName;
+        this.modelPrice = modelPrice;
+    }*/
+    set newModel(modelName) {
+        this.modelName = modelName;
+    }
+    set newPrice(modelPrice) {
+        this.modelPrice = modelPrice;
+    }
+    get companyDetails() {
+        return `Company Name: ${this.name}, Origin Country: ${this.originCountry}, Established Year: ${this.establishedYear}`;
     }
 }
+const tesla = new carCompany('Tesla', 'USA', 2003);
+console.log(typeof tesla);
+console.log(tesla);
+tesla.newModel = 'Model S';
+tesla.newPrice = 80000;
+console.log(tesla.companyDetails);
--- typescript oops/src/class.ts
+++ typescript oops/src/class.ts
@@ -1,13 +1,53 @@
-class one {
-    private _name: string;
+class carCompany {
+    name: string;
+    originCountry: string;
+    establishedYear: number;
 
-    constructor(name: string) {
-        this._name = name;
+    /*
+    // cant just do it like this
+    modelName: string;
+    modelPrice: number;
+*/
+
+    //  either values assign, or ! which tells it will be assignmend for sure
+    modelName!: string;
+    modelPrice: number = 0
+
+
+    constructor(name: string, originCountry: string, establishedYear: number) {
+        this.name = name;
+        this.originCountry = originCountry;
+        this.establishedYear = establishedYear;
     }
 
-    get name(): string {
-        return this._name;
+
+
+    // cant do it like this, must have only one agrument
+    /*
+    set newModel(modelName: string, modelPrice: number) {
+        this.modelName = modelName;
+        this.modelPrice = modelPrice;
+    }*/
+
+    set newModel(modelName: string) {
+        this.modelName = modelName;
     }
 
+    set newPrice(modelPrice: number) {
+        this.modelPrice = modelPrice;
+    }
+
+    get companyDetails() {
+        return `Company Name: ${this.name}, Origin Country: ${this.originCountry}, Established Year: ${this.establishedYear}`;
+    }
 }
 
+
+const tesla = new carCompany('Tesla', 'USA', 2003);
+console.log(typeof tesla);
+console.log(tesla)
+
+tesla.newModel = 'Model S';
+tesla.newPrice = 80000;
+
+console.log(tesla.companyDetails);
